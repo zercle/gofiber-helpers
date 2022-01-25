aab71e27e535da1e55786973bd0fd65357f35466
Kawin Viriyaprasopsook <kawin.vir@zercle.tech>
2022-01-26 01:39:36 +0700

    Add basic auth

--- context.go
+++ context.go
@@ -2,12 +2,14 @@ package helpers
 
 import (
 	"encoding/base64"
+	"net/http"
 	"net/url"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/utils"
 )
 
 // Ctx represents the context of the current HTTP request. It holds request and
@@ -267,4 +269,41 @@ func (c *Ctx) ParamBool(name string) bool {
 		v = false
 	}
 	return v
+}
+
+func (c *Ctx) BasicAuth(user, passwd string) error {
+	// Get authorization header
+	auth := c.Get(fiber.HeaderAuthorization)
+
+	// Check if the header contains content besides "basic".
+	if len(auth) <= 6 || strings.ToLower(auth[:5]) != "basic" {
+		return fiber.NewError(http.StatusUnauthorized, "invalid basic auth format")
+	}
+
+	// Decode the header contents
+	raw, err := base64.StdEncoding.DecodeString(auth[6:])
+	if err != nil {
+		return fiber.NewError(http.StatusUnauthorized, err.Error())
+	}
+
+	// Get the credentials
+	creds := utils.UnsafeString(raw)
+
+	// Check if the credentials are in the correct form
+	// which is "username:password".
+	index := strings.Index(creds, ":")
+	if index == -1 {
+		return fiber.NewError(http.StatusUnauthorized, "invalid basic auth format")
+	}
+
+	// Get the username and password
+	username := creds[:index]
+	password := creds[index+1:]
+
+	if user == username && passwd == password {
+		return c.Next()
+	}
+
+	// Authentication failed
+	return fiber.NewError(http.StatusUnauthorized, "invalid user/passwd")
 }
