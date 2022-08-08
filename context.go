51693cdaa022be2102a44bb919a0f83a0798dc17
Kawin Viriyaprasopsook <kawin.vir@zercle.tech>
2022-08-08 17:47:37 +0700

    fix: BasicAuth

--- context.go
+++ context.go
@@ -287,34 +287,35 @@ func (c *Ctx) ParamBool(name string) bool {
 
 func (c *Ctx) BasicAuth(user, passwd string) error {
 	// Get authorization header
-	auth := c.Get(fiber.HeaderAuthorization)
+	authStr := c.Get(fiber.HeaderAuthorization)
+
+	authSlices := strings.Split(authStr, " ")
+	if len(authSlices) != 2 {
+		return fiber.NewError(http.StatusUnauthorized, "invalid authorization format")
+	}
 
 	// Check if the header contains content besides "basic".
-	if len(auth) <= 6 || strings.ToLower(auth[:5]) != "basic" {
+	if strings.ToLower(authSlices[0]) != "basic" {
 		return fiber.NewError(http.StatusUnauthorized, "invalid basic auth format")
 	}
 
 	// Decode the header contents
-	raw, err := base64.StdEncoding.DecodeString(auth[6:])
+	raw, err := base64.StdEncoding.DecodeString(authSlices[1])
 	if err != nil {
 		return fiber.NewError(http.StatusUnauthorized, err.Error())
 	}
 
 	// Get the credentials
-	creds := utils.UnsafeString(raw)
+	credentialString := utils.UnsafeString(raw)
 
 	// Check if the credentials are in the correct form
 	// which is "username:password".
-	index := strings.Index(creds, ":")
-	if index == -1 {
+	credentials := strings.Split(credentialString, ":")
+	if len(credentials) != 2 {
 		return fiber.NewError(http.StatusUnauthorized, "invalid basic auth format")
 	}
 
-	// Get the username and password
-	username := creds[:index]
-	password := creds[index+1:]
-
-	if user == username && passwd == password {
+	if user == credentials[0] && passwd == credentials[1] {
 		return nil
 	}
 
